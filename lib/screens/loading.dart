f2fc8f21dce0456e6e72be895c6f06c6ee18bd53
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-08-24 17:29:26 +0900

    날씨 앱(weather app) 만들기 2 - Exception handling, Http package, Json parsing

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -10,9 +12,36 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch(e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      print(wind);
+
+      var id = jsonDecode(jsonData)['id'];
+      print(id);
+    }
   }
 
   @override
@@ -20,9 +49,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(
